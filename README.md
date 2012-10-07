3fd2a3b8f54a56ff97c89f92b53f50675ff8dde8
Kyle Terry <kyle@kyleterry.com>
2012-10-07 00:30:00 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Crap script to take unsorted music and make it look like $HOME/Music/Artist/Album/01 - Trackname.mp3
---
+++ keeper.py
@@ -0,0 +1,43 @@
+import os
+from os.path import join, exists
+import glob
+import shutil
+
+import id3reader
+
+MUSIC_DIR = '/home/kyle/unsorted_music'
+DESTINATION_DIR = '/home/kyle/Music'
+
+def main():
+    #import ipdb; ipdb.set_trace()
+    for directory in os.walk(MUSIC_DIR):
+        for _file in directory[2]:
+            _file = join(directory[0], _file)
+            try:
+                reader = id3reader.Reader(_file)
+            except IOError:
+                continue
+            if not reader.getValue('performer'):
+                print 'ERROR: skipping %s' % _file
+                continue
+            artist_album = os.path.join(DESTINATION_DIR, 
+                    reader.getValue('performer'),
+                    reader.getValue('album'))
+            track = reader.getValue('track')
+            if '/' in track:
+                track = track.split('/')[0]
+            new_file_name = '%s - %s.mp3' % (
+                track, reader.getValue('title').replace('/', ','))
+            new_file_path = join(artist_album, new_file_name)
+            if exists(new_file_path):
+                continue
+            try:
+                os.makedirs(artist_album)
+            except OSError:
+                pass
+            shutil.copy2(_file, new_file_path)
+            print 'Made: %s' % os.path.join(artist_album, new_file_name)
+
+
+if __name__ == '__main__':
+    main()
